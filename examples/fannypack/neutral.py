3cfe545a9fadecda6245cbafdc679add9f34d5f7
Wilde <wildmat@186590d2c0b7.ant.amazon.com>
2019-05-24 22:48:45 -0700

    foo

--- examples/fannypack/neutral.py
+++ examples/fannypack/neutral.py
@@ -5,9 +5,8 @@ IMAGE_BRIGHTNESS = .6
 
 NEUTRAL = get_image_data("neutral.bmp")
 def neutral():
-  for data in NEUTRAL:
-    for x in range(0, scrollphathd.DISPLAY_WIDTH):
-      for y in range(0, scrollphathd.DISPLAY_HEIGHT):
-          brightness = data[x][y]
-          scrollphathd.pixel(x, y, brightness * IMAGE_BRIGHTNESS)
-    scrollphathd.show()
+  for x in range(0, scrollphathd.DISPLAY_WIDTH):
+    for y in range(0, scrollphathd.DISPLAY_HEIGHT):
+        brightness = NEUTRAL[x][y]
+        scrollphathd.pixel(x, y, brightness * IMAGE_BRIGHTNESS)
+  scrollphathd.show()
